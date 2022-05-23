cb5bd82049b3e36e3a528f2f93d5419fef350c3d
igorc97 <igorceglarz@gmail.com>
2022-05-23 18:04:41 +0200

    Adding track

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 import * as THREE from 'https://unpkg.com/three@0.140.2/build/three.module.js';
+//import {GLTFLoader} from 'https://unpkg.com/three@0.140.2/examples/jsm/loaders/GLTFLoader.js';
+
+
+
 
 const scene = new THREE.Scene();
 const playerCar = Car();
 scene.add(playerCar);       // adding player's car to the scene
+
+
+// const loader = new GLTFLoader();
+// loader.load('objects/playerCar/scene.gltf', function(gltf){
+//     scene.add(gltf.scene);
+// }, undefined, function(error){
+//     console.error(error);
+// });
+
+//track consts
+const trackRadius = 225;
+const trackWidth = 45;
+const innerTrackRadius = trackRadius - trackWidth;
+const outerTrackRadius = trackRadius + trackWidth;
+
+const arcAngle1 = (1 / 3) * Math.PI;  // 60 deg
+
+const deltaY = Math.sin(arcAngle1) * innerTrackRadius;
+const arcAngle2 = Math.asin(deltaY / outerTrackRadius);
+
+const arcCenterX = (
+    Math.cos(arcAngle1) * innerTrackRadius +
+    Math.cos(arcAngle2) * outerTrackRadius
+) / 2;
+
+const arcAngle3 = Math.acos(arcCenterX / innerTrackRadius);
+const arcAngle4 = Math.acos(arcCenterX / outerTrackRadius);
+
 
 //adding light
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
@@ -25,13 +57,13 @@ const camera = new THREE.OrthographicCamera(
     0,  // near plane
     1000  //far plane
 );
-camera.position.set(0, 0, 300);
+camera.position.set(0, -210, 300);
 //camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
 
 //map render
 
-//renderMap(cameraWidth, cameraHeight * 2);
+renderMap(cameraWidth, cameraHeight * 2);
 
 
 //renderer !!!!
@@ -179,16 +211,132 @@ function getLineMarkings(mapWidth, mapHeight){
     return new THREE.CanvasTexture(canvas);
 }
 
+function getLeftIsland(){
+    const islandLeft = new THREE.Shape();
+    islandLeft.absarc(
+        -arcCenterX,
+        0,
+        innerTrackRadius,
+        arcAngle1,
+        -arcAngle1,
+        false
+    );
+
+    islandLeft.absarc(
+        arcCenterX,
+        0,
+        outerTrackRadius,
+        Math.PI + arcAngle2,
+        Math.PI - arcAngle2,
+        true
+    );
+    return islandLeft;
+    
+}
+
+function getMiddleIsland(){
+    const islandMiddle = new THREE.Shape();
+
+    islandMiddle.absarc(
+        -arcCenterX,
+        0,
+        innerTrackRadius,
+        arcAngle3,
+        -arcAngle3,
+        true
+    );
+
+    islandMiddle.absarc(
+        arcCenterX,
+        0,
+        innerTrackRadius,
+        Math.PI + arcAngle3,
+        Math.PI - arcAngle3,
+        true
+    );
+    return islandMiddle;
+}
+
+function getRightIsland(){
+    const islandRight = new THREE.Shape();
+
+    islandRight.absarc(
+        arcCenterX,
+        0,
+        innerTrackRadius,
+        Math.PI - arcAngle1,
+        Math.PI + arcAngle1,
+        true
+    );
+
+    islandRight.absarc(
+        -arcCenterX,
+        0,
+        outerTrackRadius,
+        -arcAngle2,
+        arcAngle2,
+        false
+    );
+
+    return islandRight;
+}
+
+function getOuterField(mapWidth, mapHeight){
+    const field = new THREE.Shape();
+
+    field.moveTo(-mapWidth / 2, -mapHeight / 2);
+    field.lineTo(0, -mapHeight / 2);
+
+    field.absarc(
+        -arcCenterX,
+        0,
+        outerTrackRadius,
+        -arcAngle4,
+        arcAngle4,
+        true
+    );
+
+    field.absarc(
+        arcCenterX,
+        0,
+        outerTrackRadius,
+        Math.PI - arcAngle4,
+        Math.PI + arcAngle4,
+        true
+    );
+
+    field.lineTo(0, -mapHeight / 2);
+    field.lineTo(mapWidth / 2, -mapHeight / 2);
+    field.lineTo(mapWidth / 2, mapHeight / 2);
+    field.lineTo(-mapWidth / 2, mapHeight / 2);
+    return field;
+}
 
 function renderMap(mapWidth, mapHeight){
     const lineMarkingsTexture = getLineMarkings(mapWidth, mapHeight);
     const planeGeometry  = new THREE.PlaneBufferGeometry(mapWidth, mapHeight);
     const planeMaterial  = new THREE.MeshLambertMaterial({
-        color: 0x546e90,
         map: lineMarkingsTexture,
     });
     const plane = new THREE.Mesh(planeGeometry, planeMaterial);
     scene.add(plane);
+
+    //extruded geometry
+
+    const islandLeft = getLeftIsland();
+    const islandRight = getRightIsland();
+    const islandMiddle = getMiddleIsland();
+    const outerField  = getOuterField(mapWidth, mapHeight);
+    
+    const fieldGeometry = new THREE.ExtrudeBufferGeometry(
+        [islandLeft, islandMiddle, islandRight, outerField],
+        {depth: 6, bevelEnabled: false}
+    );
+    const fieldMesh = new THREE.Mesh(fieldGeometry,[
+        new THREE.MeshLambertMaterial({ color: 0x67c240}),
+        new THREE.MeshLambertMaterial({ color: 0x23311c}),
+    ]);
+    scene.add(fieldMesh);
 }
 
 function pickRandom(array){              /// dodac kolory? tak zeby randomowo dawalo kolory na auta itd
